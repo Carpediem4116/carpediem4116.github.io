eeae35b02091235479b1da25785ca99d59c63a05
Carpediem <a1446008821@proton.me>
2024-08-04 10:20:32 +0800

    Site updated: 2024-08-04 10:20:32

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/07/31/My-New-Post/","2024/08/03/hello-world/","2024/08/03/2/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
