2b54fb36f14ae66f66b236ebb914a516c090b4bb
Carpediem <a1446008821@proton.me>
2024-08-13 19:51:20 +0800

    Site updated: 2024-08-13 19:51:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/03/hello-world/","2024/08/11/函数/"];function toRandomPost(){
+var posts=["2024/08/03/hello-world/","2024/08/12/inequality/","2024/08/12/funcrion/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
