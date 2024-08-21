42802a6182fbd26b529b29b700ac30882ca203e3
Carpediem <a1446008821@proton.me>
2024-08-21 14:06:17 +0800

    Site updated: 2024-08-21 14:06:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/03/hello-world/","2024/08/12/inequality/","2024/08/12/funcrion/","2024/08/14/Elementary number theory/"];function toRandomPost(){
+var posts=["2024/08/12/funcrion/","2024/08/03/hello-world/","2024/08/12/inequality/","2024/08/14/Elementary number theory/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
